1775bef49ad241b5114c5dc80bd9096141373ff4
Jessica Caramiello <jxc1399@student.bham.ac.uk>
2023-07-27 22:45:39 +0100

    Added an input box

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import Image from 'next/image'
+import { useState } from 'react'
 
 export default function Home() {
+  const [toDoList, setToDoList]= useState(null);
   return (
     <>
-    
+      <div>
+        <label htmlFor="newEntry">New To Do: </label>
+        <input type='text' id='newEntry' name='newEntry'></input>
+      </div>
     </>
   )
 }
