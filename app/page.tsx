f2326bed056799a2fcbf34e7e98b563fae19de35
Jessica Caramiello <jxc1399@student.bham.ac.uk>
2023-07-30 18:53:57 +0100

    Created add item and delete item functions

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,56 @@
 'use client'
-import Image from 'next/image'
-import { useState } from 'react'
-
+import React, {useState } from 'react'
+interface ToDoItem {
+  id: number;
+  value: string;
+}
 export default function Home() {
-  const [toDoList, setToDoList]= useState(null);
+
+  const [item, setItem] = useState("");
+  const [toDoList, setToDoList] = useState<ToDoItem[]>([]);
+
+  function addItem() {
+    if(!item){alert("you have not entered an item")
+  return;}
+
+    const newItem: ToDoItem = {
+      id: Math.floor(Math.random() * 1000),
+      value: item,
+    };
+    setToDoList([...toDoList, newItem]);
+    setItem("");
+  }
+
+  function deleteItem(id: number)
+  {
+      const temp = toDoList.filter(item=>item.id !=id);
+      setToDoList(temp);
+  }
+  
+  
+
   return (
     <>
       <div>
         <label htmlFor="newEntry">New ToDo: </label>
-        <input type='text' id='newEntry' name='newEntry'></input>
-        <button>Add new ToDo</button>
+        <input type='text' 
+                placeholder='Add an item' 
+                id='newEntry' 
+                name='newEntry'
+                value={item}
+                onChange={e => setItem(e.target.value)}
+        />
+        <button onClick={()=>addItem()}>Add</button>
+      </div>
+      <div>
+        <ul>
+          {toDoList.map(toDo=>{
+             return(
+              <li key={toDo.id}>{toDo.value} <button onClick={()=>deleteItem(toDo.id)}>❌</button></li>
+             )  
+             }
+            )}
+        </ul>
       </div>
     </>
   )
